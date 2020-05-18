9d7ea92137c0e1f90c4c0b8a542ac95f650c9d99
Travis Pfeifer <tdpfeifer@outlook.com>
2020-05-17 22:32:11 -0500

    Front Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.css';
 import FrontPage from './Components/FrontPage';
 import ButtonAppBar from './Components/ButtonAppBar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
-const App = () => ( 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#558b2f',
+    },
+    secondary: {
+      main: '#536dfe',
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}> 
     <>
     <ButtonAppBar />
     <FrontPage />
-    </> 
+    </>
+    </ThemeProvider>
 );
 
 export default App;
