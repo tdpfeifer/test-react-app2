3830a0ac004552f4cfed96a30a5f424a1944cbdb
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2020-05-19 14:59:17 -0500

    eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+      ecmaFeatures: { jsx: true, modules: true },
+    },
+    env: {
+      browser: true,
+      es6: true,
+      node: true,
+      mocha: true,
+      "jest/globals": true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended",
+      "prettier/react",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:jsx-a11y/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:jest/recommended",
+    ],
+    plugins: ["prettier", "import", "react", "jsx-a11y", "react-hooks", "jest"],
+    rules: {
+      "no-console": "off",
+      "prettier/prettier": ["warn", { singleQuote: true, bracketSpacing: false }],
+      "dot-notation": "warn",
+      "quote-props": ["warn", "as-needed"],
+      "arrow-body-style": ["warn", "as-needed"],
+      "object-shorthand": "warn",
+      "react/prop-types": "off",
+    },
+    settings: { react: { version: "detect" } },
+  };
