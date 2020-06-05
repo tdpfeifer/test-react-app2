c4184dca898a5fd0be0919951123a815db31bfc0
Stephen Salyer <39383255+stephen-salyer@users.noreply.github.com>
2020-06-04 21:42:25 -0500

    Things we will change later

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,9 +3,14 @@ import {Grid, Typography, Box} from '@material-ui/core';
 import Product1 from '../Img/Product-1.png';
 import {ProductsList} from './ListOfProducts';
 
-export default function Product() {
-  const first = ProductsList[0];
+const first = ProductsList[0];
+var firstName = first.name;
+var firstSize = first.size;
+var firstMaker = first.maker;
+var firstPrice = first.price;
+var firstRating = first.rating;
 
+export default function Product() {
   return (
     <Grid container spacing={3}>
       <>
@@ -21,11 +26,15 @@ export default function Product() {
             <img src={Product1} alt="" style={{width: '60%'}} />
           </Box>
         </Grid>
-        {ProductsList.map(({first(name)},i) => (
-          <Grid item xs={12} md={5} key={first.name}>
-            <Typography>{name}</Typography>
-          </Grid>
-        ))}
+        <Grid item xs={12} md={5}>
+          <Box pt={3} display="flex" flexDirection="column">
+            <Typography variant="h4">{firstName}</Typography>
+            <Typography variant="overline">{firstSize}</Typography>
+            <Typography variant="overline">{firstMaker}</Typography>
+            <Typography variant="overline">{firstPrice}</Typography>
+            <Typography variant="overline">{firstRating}</Typography>
+          </Box>
+        </Grid>
       </>
     </Grid>
   );
